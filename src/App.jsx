8549360adea7bd3ca2856b74f51d2f3e9afdffd4
piotrpdev <piotrpdev@gmail.com>
2024-01-10 00:57:53 +0000

    feat: updated projects, certs, courses, work

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,18 @@ import ContactIconWrapper from "./components/ContactIconWrapper";
 import Results from "./components/Results";
 
 function App() {
-  const [flipPage, setFlipPage] = React.useState(true);
+  const [flipPage, setFlipPage] = React.useState(false);
 
   return (
     <>
       <aside>
-        <button type="button" onClick={() => setFlipPage(prev => !prev)} aria-label="Flip Page"><FaRotate /></button>
+        <button
+          type="button"
+          onClick={() => setFlipPage((prev) => !prev)}
+          aria-label="Flip Page"
+        >
+          <FaRotate />
+        </button>
       </aside>
       {flipPage ? (
         <>
@@ -43,6 +49,7 @@ function App() {
                 <ContactIconWrapper
                   href="tel:***REMOVED***"
                   icon={<FaPhoneAlt />}
+                  // @ts-ignore
                   text={import.meta.env.VITE_PHONE_NUMBER || <b>REDACTED</b>}
                 />
                 <ContactIconWrapper
@@ -91,7 +98,6 @@ function App() {
           </section>
 
           <section>
-            {/* TODO: Update these */}
             <SectionHeading>Projects</SectionHeading>
 
             <Heading
@@ -105,15 +111,35 @@ function App() {
             </Heading>
             <div className="indentedContent project">
               <p>
-                Controlling <ExtLinkText text="Discord" href="https://discord.com/safety/360044149331-what-is-discord" /> (VoIP desktop app) using a combination of <ExtLinkText text="Arduino" href="https://learn.sparkfun.com/tutorials/what-is-an-arduino/all" />, <ExtLinkText text="BetterDiscord" href="https://betterdiscord.app/" />, and Python.
+                Controlling{" "}
+                <ExtLinkText
+                  text="Discord"
+                  href="https://discord.com/safety/360044149331-what-is-discord"
+                />{" "}
+                (VoIP desktop app) using a combination of{" "}
+                <ExtLinkText
+                  text="Arduino"
+                  href="https://learn.sparkfun.com/tutorials/what-is-an-arduino/all"
+                />
+                ,{" "}
+                <ExtLinkText
+                  text="BetterDiscord"
+                  href="https://betterdiscord.app/"
+                />
+                , and Python.
               </p>
               <ul>
                 <li>
-                  Created a touchscreen user interface and simple messaging protocol on the Arduino using C++.
+                  Created a touchscreen user interface and simple messaging
+                  protocol on the Arduino using C++.
                 </li>
-                <li>Made a Python WebSocket server to facilitate communication between the Arduino and Discord.</li>
                 <li>
-                  Reverse-engineered Discord to capture UI events and send them to the Python Server using JavaScript.
+                  Made a Python WebSocket server to facilitate communication
+                  between the Arduino and Discord.
+                </li>
+                <li>
+                  Reverse-engineered Discord to capture UI events and send them
+                  to the Python Server using JavaScript.
                 </li>
               </ul>
             </div>
@@ -127,7 +153,7 @@ function App() {
                 <ExtLink />
               </HeadingTitle>
             </Heading>
-            
+
             <div className="indentedContent project">
               <p>
                 Uses <ExtLinkText text="Puppeteer" href="https://pptr.dev/" />
@@ -152,8 +178,8 @@ function App() {
                   including Webhooks and uploading files.
                 </li>
                 <li>
-                  Used YAML and understanding of Linux to create effective scripts
-                  for Virtual Machines.
+                  Used YAML and understanding of Linux to create effective
+                  scripts for Virtual Machines.
                 </li>
               </ul>
             </div>
@@ -185,7 +211,7 @@ function App() {
                 </li>
               </ul>
             </div> */}
-            
+
             <Heading
               href="https://cao-calculator.vercel.app/"
               aside="Dec 2020 &#8211; Feb 2021"
@@ -203,15 +229,16 @@ function App() {
               <ul>
                 <li>
                   Went through the whole process of coming up with an idea,
-                  designing an app, prototyping, and publishing the final product.
+                  designing an app, prototyping, and publishing the final
+                  product.
                 </li>
                 <li>
                   Used Expo, React Native, TypeScript, and React Native Paper
                 </li>
                 <li>
                   Worked on the project on a regular basis, avoiding scope creep
-                  and using a Kanban board to focus on finishing the product in a
-                  timely manner.
+                  and using a Kanban board to focus on finishing the product in
+                  a timely manner.
                 </li>
               </ul>
             </div>
@@ -220,17 +247,22 @@ function App() {
       ) : (
         <>
           <section>
-            {/* TODO: Update these */}
             <SectionHeading>Skills</SectionHeading>
             <div className="skills">
               <table>
                 <tr>
                   <th>Technology</th>
-                  <td>HTML5, CSS3, JavaScript, TypeScript, React, Svelte, Python, Java, PHP, Docker</td>
+                  <td>
+                    HTML5, CSS3, JavaScript/TypeScript, React, Python,{" "}
+                    Java/Kotlin, Go, C/C#/C++, PHP, Docker
+                  </td>
                 </tr>
                 <tr>
                   <th>Tools</th>
-                  <td>GitHub, VSCode, Figma, Microsoft Teams and Office, Trello, Slack, LaTeX</td>
+                  <td>
+                    GitHub/Git, VSCode, Figma, MS Teams and Office, Trello,
+                    Slack, JIRA, LaTeX
+                  </td>
                 </tr>
                 <tr>
                   <th>Languages</th>
@@ -245,8 +277,18 @@ function App() {
             <div className="certs">
               <table>
                 <tr>
-                  <th><ExtLinkText text="AWS Certified Solutions Architect" href="https://www.credly.com/badges/3f71b700-5869-4486-a00e-8f954a4fabbc/linked_in_profile" /></th>
-                  <th><ExtLinkText text="AWS Certified Cloud Practitioner" href="https://www.credly.com/badges/753358be-51cd-4817-897b-ae7dab4ddc3d/linked_in_profile" /></th>
+                  <th>
+                    <ExtLinkText
+                      text="AWS Certified Solutions Architect"
+                      href="https://www.credly.com/badges/3f71b700-5869-4486-a00e-8f954a4fabbc/linked_in_profile"
+                    />
+                  </th>
+                  <th>
+                    <ExtLinkText
+                      text="AWS Certified Cloud Practitioner"
+                      href="https://www.credly.com/badges/753358be-51cd-4817-897b-ae7dab4ddc3d/linked_in_profile"
+                    />
+                  </th>
                 </tr>
                 <tr>
                   <td>Completed Aug 2022 - Expires Aug 2025</td>
@@ -257,12 +299,52 @@ function App() {
           </section>
 
           <section>
-            {/* TODO: Add the OpenShift and Kafka course, maybe the Golang course? */}
             <SectionHeading>Courses</SectionHeading>
+
             <Heading
               className="heading inline"
+              href="https://www.redhat.com/en/services/training/ad482-developing-event-driven-applications-apache-kafka-and-red-hat-amq-streams"
+              aside="July 2023"
+            >
+              <HeadingTitle>
+                Event-Driven Applications with Apache Kafka (AD482),{" "}
+              </HeadingTitle>
+              <HeadingSubtitle>
+                Red Hat Training and Certification
+              </HeadingSubtitle>
+            </Heading>
+
+            <div className="indentedContent">
+              <p>
+                Learned to work with Apache Kafka and Red Hat AMQ Streams to
+                create event-driven applications.
+              </p>
+            </div>
+
+            <Heading
+              className="heading inline"
+              href="https://www.redhat.com/en/services/training/red-hat-openshift-administration-i-operating-a-production-cluster"
+              aside="Jun 2023"
+            >
+              <HeadingTitle>
+                Red Hat OpenShift Administration I (DO180),{" "}
+              </HeadingTitle>
+              <HeadingSubtitle>
+                Red Hat Training and Certification
+              </HeadingSubtitle>
+            </Heading>
+
+            <div className="indentedContent">
+              <p>
+                Learned to manage OpenShift Container Platform clusters along
+                with applications running on them.
+              </p>
+            </div>
+
+            {/* <Heading
+              className="heading inline"
               href="https://www.netacad.com/courses/os-it/ndg-linux-essentials"
-              aside="Sep 2021 &#8211; Mar 2022"
+              aside="Sep 2021"
             >
               <HeadingTitle>
                 NDG Linux Essentials,{" "}
@@ -274,36 +356,75 @@ function App() {
 
             <div className="indentedContent">
               <p>
-                Learned the important parts of working with Linux including scipting, archiving/compression, etc.
+                Learned the important parts of working with Linux including scripting, archiving/compression, etc.
               </p>
-            </div>
+            </div> */}
           </section>
 
           <section>
-            {/* TODO: Update this and add Red Hat, maybe remove kelly's for space? Description length? Add logo */}
             <SectionHeading>Professional Experience</SectionHeading>
+
             <Heading
               className="heading inline"
-              href="https://www.kellys.ie/"
-              aside="Aug 2021 &#8211; Present"
+              href="https://www.redhat.com/en"
+              aside="Jun 2023 &#8211; Dec 2023"
             >
-              <HeadingTitle>
-                Wash-up Personnel,{" "}
-              </HeadingTitle>
-              <HeadingSubtitle>
-                Kelly's Resort Hotel, Rosslare
-              </HeadingSubtitle>
+              <HeadingTitle>Software Engineering Intern, </HeadingTitle>
+              <HeadingSubtitle>Red Hat, Waterford</HeadingSubtitle>
             </Heading>
 
             <div className="indentedContent">
               <p>
-                Consistently able to perform under pressure, work efficiently with a team, and arrive/leave on time.
+                Part of the Red Hat OpenShift AI Edge team. Learned how to use{" "}
+                <ExtLinkText
+                  text="OpenShift"
+                  href="https://www.redhat.com/en/technologies/cloud-computing/openshift"
+                />
+                , worked on the RHOAI Edge PoC, and also completed several
+                smaller side projects involving Kubernetes and Go.
+              </p>
+              <p>
+                For the PoC, I mainly contributed to the{" "}
+                <ExtLinkText
+                  text="OpenShift Pipelines"
+                  href="https://docs.openshift.com/pipelines/1.13/about/understanding-openshift-pipelines.html"
+                />
+                /<ExtLinkText text="Tekton" href="https://tekton.dev/" /> code
+                which was used to package a provided{" "}
+                <ExtLinkText
+                  text="AI/ML model"
+                  href="https://www.redhat.com/en/blog/what-aiml-and-why-does-it-matter-your-business"
+                />{" "}
+                along with its dependencies into a container image, test that
+                container image, and finally upload it to{" "}
+                <ExtLinkText text="Quay" href="https://quay.io/" /> for
+                deployment to the{" "}
+                <ExtLinkText
+                  text="Edge"
+                  href="https://www.redhat.com/en/products/edge"
+                />
+                . Most of my work can be viewed in the <ExtLinkText text="Open Data Hub AI Edge GitHub repository" href="https://github.com/opendatahub-io/ai-edge" />, I also wrote <ExtLinkText text="two Medium articles" href="https://medium.com/piotr-p%C5%82aczeks-red-hat-internship-experience/new-journey-at-red-hat-c964da99b3c7" /> in which I talk about my internship.
+              </p>
+            </div>
+
+            <Heading
+              className="heading inline"
+              href="https://www.kellys.ie/"
+              aside="Aug 2021 &#8211; Dec 2022"
+            >
+              <HeadingTitle>Kitchen Porter, </HeadingTitle>
+              <HeadingSubtitle>Kelly&apos;s Resort Hotel, Rosslare</HeadingSubtitle>
+            </Heading>
+
+            <div className="indentedContent">
+              <p>
+                Consistently performed well under pressure, worked efficiently
+                with a team, and arrived/left on time.
               </p>
             </div>
           </section>
 
-          <section>
-            {/* TODO: Remove this for space? */}
+          {/* <section>
             <SectionHeading>Volunteering Experience</SectionHeading>
             <Heading
               className="heading inline"
@@ -323,7 +444,7 @@ function App() {
 helping with the cleaning of my town and landscaping.
               </p>
             </div>
-          </section>
+          </section> */}
 
           <section>
             {/* TODO: Condense/Remove two for space? */}
@@ -335,16 +456,29 @@ helping with the cleaning of my town and landscaping.
                   <th>Flight Simulation</th>
                 </tr>
                 <tr>
-                  <td>I like messing around with the Arduino and Raspberry Pi. My most notable project is a throttle quadrant for flight sims.</td>
-                  <td>I like playing Microsoft Flight Simulator, listening to ATC traffic, reading airplane manuals etc. I also like airplane related documentaries.</td>
+                  <td>
+                    I like messing around with the Arduino and Raspberry Pi. My
+                    most notable project is a throttle quadrant for flight sims.
+                  </td>
+                  <td>
+                    I like playing Microsoft Flight Simulator, listening to ATC
+                    traffic, reading airplane manuals etc. I also like airplane
+                    related documentaries.
+                  </td>
                 </tr>
                 <tr>
                   <th>Gaming</th>
                   <th>Music</th>
                 </tr>
                 <tr>
-                  <td>I like playing milsims such as Arma 3 along with some casual games such as Rocket League and Pokemon.</td>
-                  <td>I love all aspects of music: listening, composition, general theory, etc.</td>
+                  <td>
+                    I like playing milsims such as Arma 3 along with some casual
+                    games such as Rocket League and Pokemon.
+                  </td>
+                  <td>
+                    I love all aspects of music: listening, composition, general
+                    theory, etc.
+                  </td>
                 </tr>
               </table>
             </div>
@@ -355,7 +489,7 @@ helping with the cleaning of my town and landscaping.
             <div className="references">
               <table>
                 <tr>
-                  <th>Brendan O'Farrell</th>
+                  <th>Brendan O&apos;Farrell</th>
                   <th>Mary Lyng</th>
                   <th>Niamh Lambert</th>
                 </tr>
@@ -367,7 +501,7 @@ helping with the cleaning of my town and landscaping.
                 <tr>
                   <td>Red Hat</td>
                   <td>South East Technological University</td>
-                  <td>Kelly’s Resort Hotel & Spa</td>
+                  <td>Kelly&apos;s Resort Hotel & Spa</td>
                 </tr>
                 <tr>
                   <td>bofarrel@redhat.com</td>
@@ -375,7 +509,12 @@ helping with the cleaning of my town and landscaping.
                   <td>NLambert@kellys.ie</td>
                 </tr>
                 <tr>
-                  <td>{import.meta.env.VITE_BRENDAN_PHONE_NUMBER || <b>REDACTED</b>}</td>
+                  <td>
+                    {/* @ts-ignore */}
+                    {import.meta.env.VITE_BRENDAN_PHONE_NUMBER || (
+                      <b>REDACTED</b>
+                    )}
+                  </td>
                   <td>+353 51 302 057</td>
                   <td>+353 53 913 2114</td>
                 </tr>
